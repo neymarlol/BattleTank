09f2a11be42acac4262fbba22ec05de1dca7b5bd
sanjay <sanjay.nadimpally@gmail.com>
2019-08-07 16:33:09 +1000

    #Commit 1
    * Project created

---
+++ BattleTank_Proj/Source/BattleTank_Proj/BattleTank_ProjGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BattleTank_ProjGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETANK_PROJ_API ABattleTank_ProjGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
